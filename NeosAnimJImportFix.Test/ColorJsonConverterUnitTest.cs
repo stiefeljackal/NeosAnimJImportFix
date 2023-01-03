dfccc0f769e3d9a2c80c836a85ed3d31a968d92b
StiefelJackal <stiefel@jackalworkz.net>
2023-01-03 16:31:45 -0500

    test: add unit tests for color

---
+++ NeosAnimJImportFix.Test/ColorJsonConverterUnitTest.cs
@@ -0,0 +1,78 @@
+﻿using FrooxEngine;
+using Moq;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using Xunit;
+using BepuPhysics;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BaseX;
+using System.Text;
+using JworkzNeosMod.JsonConverters;
+using JworkzNeosMod.Extensions;
+
+namespace JworkzNeosMod.Test
+{
+    interface ITest
+    {
+        ISyncBoolMock EnabledField { get; set; }
+    }
+    interface ISyncBoolMock
+    {
+        bool IsDriven { get; set;}
+    }
+    public class ColorJsonConverterUnitTest
+    {
+        private static readonly string[] COLOR_PROP_NAMES = new string[] { "r", "g", "b", "a" };
+
+
+        [Theory(DisplayName = "Should read the JSON of BaseX.color as an object type.")]
+        [MemberData(nameof(GetData), parameters: 9)]
+        public void ReadJsonValueOfColorAsObject(string json, color expectedColor)
+        {
+            var converter = new ColorJsonConverter();
+            var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(json));
+            reader.Read();
+
+            Assert.Equal(expectedColor, converter.Read(ref reader, typeof(color), null));
+        }
+
+        public static string CreateColorValueJson(float?[] rgbaArr)
+        {
+            var sb = new StringBuilder("{");
+
+            for (var i = 0; i < rgbaArr.Length; i++)
+            {
+                var floatNullable = rgbaArr[i];
+                if (!floatNullable.HasValue) { continue;  }
+                sb.Append($"\"{COLOR_PROP_NAMES[i]}\": ");
+                sb.Append(floatNullable.Value);
+                if (i < rgbaArr.Length - 1) { sb.Append(','); }
+            }
+
+            sb.AppendLine("}");
+            return sb.ToString();
+        }
+
+        public static IEnumerable<object[]> GetData(int numTests)
+        {
+            var floatData = new List<float?[]>()
+            {
+                new float?[] { 1, 2, 3, 4 },
+                new float?[] { 1 },
+                new float?[] { 1, 2 },
+                new float?[] { 1, 2, 3 },
+                new float?[] { null, 1 },
+                new float?[] { null, null, 1 },
+                new float?[] { null, null, null, 1 },
+                new float?[] { 1, null, 2 },
+                new float?[] { 1, null, null, 2 }
+            }.Select(data => new object[] { CreateColorValueJson(data), data.ToColorValue() });
+
+            return floatData.Take(numTests);
+        }
+    }
+}
--- NeosAnimJImportFix.Test/Properties/AssemblyInfo.cs
+++ NeosAnimJImportFix.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NeosAutoAttachIsPlayingDriver.Test")]
+[assembly: AssemblyTitle("JworkzNeosAnimJImportFix.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NeosAutoAttachIsPlayingDriver.Test")]
+[assembly: AssemblyProduct("JworkzNeosAnimJImportFix.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
